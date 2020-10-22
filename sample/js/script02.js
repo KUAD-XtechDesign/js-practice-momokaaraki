7bc8c78076a485e276a67e2513ff2bb8579ecdb4
momokaaraki <68629509+momokaaraki@users.noreply.github.com>
2020-10-22 17:29:48 +0900

    変更

---
+++ sample/js/script02.js
@@ -0,0 +1,28 @@
+$(function(){
+
+let txt1 = "おはようございます！";
+let txt2 = "おげんきですか？";
+let num1 = 20;
+let num2 = 32;
+
+$("#button01").on("click",function(){
+    $("#content").text("こんにちは！");
+})
+
+$("#button2").on("click",function(){
+    $("#content").text(txt1);
+})
+
+$("#button3").on("click",function(){
+    $("#content").text(txt1+text2);
+})
+
+$("#button04").on("click",function(){
+    $("#content").text(num1+num2);
+})
+
+$("#button05").on("click,function"(){
+    $("#content").text(num1+ "たす" + num2 + "は" + (num1 + num2) + "です");
+})
+
+});
